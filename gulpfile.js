c39729ec5e7a1ea80a876ff700c008969b46d7ce
fast1sh <34493866+fast1sh@users.noreply.github.com>
2018-09-24 16:31:34 +0300

    added gulp-wait

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,10 @@
 var gulp 						= require('gulp');
 var browserSync 				= require('browser-sync');
 var sass        				= require('gulp-sass');
-var autoprefixer 			    = require('gulp-autoprefixer')
+var autoprefixer 			    = require('gulp-autoprefixer');
 var concatCSS 					= require('gulp-concat-css');
 var ftp 						= require('gulp-ftp');
+var wait                        = require('gulp-wait');
 
 // Static Server + watching scss/html files
 gulp.task('serve', ['sass'], function() {
@@ -19,6 +20,7 @@ gulp.task('serve', ['sass'], function() {
 // Compile sass into CSS & auto-inject into browsers
 gulp.task('sass', function() {
     return gulp.src("src/sass/*.scss")
+        .pipe(wait(500))
         .pipe(sass().on('error', sass.logError))
         .pipe(autoprefixer({
             browsers: ['last 2 versions'],
